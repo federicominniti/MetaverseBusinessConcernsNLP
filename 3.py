2a8f1bdbf6064f7701daea3ddbf6e2ac2229b011
Federico Minniti <93187746+federicominniti@users.noreply.github.com>
2022-06-21 12:19:19 +0200

    updated nlp stages and added classifier

--- 3.py
+++ 3.py
@@ -1,56 +1,20 @@
-# STEP 3: NLP(TOKENIZATION, NORMALIZATION, FILTERING, STEMMING, DESTEMMING)
-import re
-from wordcloud import WordCloud
-from unidecode import unidecode
-from collections import Counter
-from nltk.corpus import stopwords
-from nltk.stem.porter import *
+from transformers import pipeline
+sentiment_analysis = pipeline("sentiment-analysis",model="siebert/sentiment-roberta-large-english")
 
-# create the wordcloud
-def create_wordcloud(tweet_counter, filename):
-    tweet_cloud = WordCloud(background_color = "white",
-                        width = 1000,
-                        height = 500,
-                        relative_scaling = 0.5,
-                        normalize_plurals = False).generate_from_frequencies(dict(tweet_counter.most_common(100)))
-    tweet_cloud.to_file("./imgs/{}.png".format(filename))
+# 0 -> positive
+# 1 -> neutral
+# 2 -> negative
 
-STOPWORDS_EN = set(stopwords.words("english"))
-STOPWORDS_EN.add('http')
-STOPWORDS_EN.add('https')
-STEMMER_EN = PorterStemmer()
+tweets_dataframe["sentiment"] = 1
 
-formatted_tweets = tweets_dataframe.to_dict('records')
+def int_sentiment(row):
+    res = sentiment_analysis(row["Text"])
+    if res[0]["score"] > 0.6:
+        if res[0]["label"] == 'POSITIVE':
+            return 0
+        else:
+            return 2
 
-merged_splitted = []
-merged_normalized = []
-merged_filtered = []
-merged_stemmed = []
-merged_destemmed = []
+tweets_dataframe["sentiment"] = tweets_dataframe.apply(lambda row : int_sentiment(row), axis= 1)
 
-for i in formatted_tweets:
-    tokens = re.split('\W+', i["Text"], flags=re.UNICODE)
-    normalized = [unidecode(t.lower()) for t in tokens]
-    filtered = [t for t in normalized if len(t) >= 3 and t not in STOPWORDS_EN]
-    stemmed = [STEMMER_EN.stem(t) for t in filtered]
-
-    stem_mapping = {}
-    for f in filtered:
-        stemmed_f = STEMMER_EN.stem(f)
-        if stemmed_f not in stem_mapping:
-            stem_mapping[stemmed_f] = Counter()
-        stem_mapping[stemmed_f].update([f])
-
-    i["tokens"] = [stem_mapping[t].most_common(1)[0][0] for t in stemmed]
-
-    merged_splitted = merged_splitted + tokens
-    merged_normalized = merged_normalized + normalized
-    merged_filtered = merged_filtered + filtered
-    merged_stemmed = merged_stemmed + stemmed
-    merged_destemmed = merged_destemmed + i["tokens"]
-
-create_wordcloud(Counter(merged_splitted), "wordcloud_1")
-create_wordcloud(Counter(merged_normalized), "wordcloud_2")
-create_wordcloud(Counter(merged_filtered), "wordcloud_3")
-create_wordcloud(Counter(merged_stemmed), "wordcloud_4")
-create_wordcloud(Counter(merged_destemmed), "wordcloud_5")
+tweets_dataframe.to_csv('classified_tweets.csv', sep=',', index=False)
--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+import string
+
 import snscrape.modules.twitter as sntwitter
 import pandas as pd
 import botometer
@@ -57,8 +59,104 @@ if len(sys.argv) == 5:
     tweets_dataframe = pd.DataFrame(tweet_list, columns=['Datetime', 'Tweet Id', 'Text', 'Username'])
     tweets_dataframe.to_csv('text-query-tweets.csv', sep=',', index=False)
 else:
-    tweets_dataframe = pd.read_csv('tweets-final.csv')
+    tweets_dataframe = pd.read_csv('tweets_final.csv')
 
 print("done!")
 # STEP 2: REMOVE TWEETS FROM BOTS
 #tweets_dataframe = remove_bot_tweets(tweets_dataframe)
+
+# STEP 3: NLP(TOKENIZATION, NORMALIZATION, FILTERING, STEMMING, DESTEMMING)
+import re
+from wordcloud import WordCloud
+from unidecode import unidecode
+from collections import Counter
+from nltk.corpus import stopwords
+from nltk.stem.porter import *
+
+# create the wordcloud
+def create_wordcloud(tweet_counter, filename):
+    tweet_cloud = WordCloud(background_color = "white",
+                        width = 1000,
+                        height = 500,
+                        relative_scaling = 0.5,
+                        normalize_plurals = False).generate_from_frequencies(dict(tweet_counter.most_common(100)))
+    tweet_cloud.to_file("./imgs/{}.png".format(filename))
+
+def clean_text(text):
+    # Remove twitter handlers
+    text = re.sub('@[^\s]+', '', text)
+    # remove hashtags
+    text = re.sub(r'\B#\S+', '', text)
+    # Remove all the special characters
+    text = ' '.join(re.findall(r'\w+', text))
+    # remove all single characters
+    text = re.sub(r'\s+[a-zA-Z]\s+', '', text)
+    # Substituting multiple spaces with single space
+    text = re.sub(r'\s+', ' ', text, flags=re.I)
+
+    #Make text lowercase, remove text in square brackets, remove punctuation and remove words containing numbers.
+    text = text.lower()
+    text = re.sub('\[.*?\]', '', text)
+    text = re.sub('[%s]' % re.escape(string.punctuation), '', text)
+    text = re.sub('\w*\d\w*', '', text)
+
+    #Get rid of some additional punctuation and non-sensical text that was missed the first time around.
+    text = re.sub('[‘’“”…]', '', text)
+    text = re.sub('\n', '', text)
+
+    return text
+
+def readlines():
+    stopwords_file = open('stopwords.txt','r')
+    lines = stopwords_file.readlines()
+    # Strips the newline character
+    stopwords = []
+    for line in lines:
+        stopwords.append(line.strip())
+    return stopwords
+
+STOPWORDS_EN = set(stopwords.words("english"))
+#file_stopwords = readlines()
+#STOPWORDS_EN.union(file_stopwords)
+
+STEMMER_EN = PorterStemmer()
+#tweets_dataframe = tweets_dataframe[0:200]
+formatted_tweets = tweets_dataframe.to_dict('records')
+
+merged_splitted = []
+merged_normalized = []
+merged_filtered = []
+merged_stemmed = []
+merged_destemmed = []
+
+for i in formatted_tweets:
+    if "http" not in i["Text"]:
+        clean_text(i["Text"])
+        tokens = re.split('\W+', i["Text"], flags=re.UNICODE)
+        normalized = [unidecode(t.lower()) for t in tokens]
+        filtered = [t for t in normalized if len(t) >= 3 and t not in STOPWORDS_EN]
+        stemmed = [STEMMER_EN.stem(t) for t in filtered]
+
+        stem_mapping = {}
+        for f in filtered:
+            stemmed_f = STEMMER_EN.stem(f)
+            if stemmed_f not in stem_mapping:
+                stem_mapping[stemmed_f] = Counter()
+            stem_mapping[stemmed_f].update([f])
+
+        i["tokens"] = [stem_mapping[t].most_common(1)[0][0] for t in stemmed]
+
+        merged_splitted = merged_splitted + tokens
+        merged_normalized = merged_normalized + normalized
+        merged_filtered = merged_filtered + filtered
+        merged_stemmed = merged_stemmed + stemmed
+        merged_destemmed = merged_destemmed + i["tokens"]
+
+v = Counter(merged_destemmed)
+print(v)
+
+#create_wordcloud(Counter(merged_splitted), "wordcloud_1")
+#create_wordcloud(Counter(merged_normalized), "wordcloud_2")
+#create_wordcloud(Counter(merged_filtered), "wordcloud_3")
+#create_wordcloud(Counter(merged_stemmed), "wordcloud_4")
+#create_wordcloud(Counter(merged_destemmed), "wordcloud_5")

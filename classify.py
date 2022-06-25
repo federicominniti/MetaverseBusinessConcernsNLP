668ae9c61a54ac1deff736ee76af3ccd8666921a
Federico Minniti <93187746+federicominniti@users.noreply.github.com>
2022-06-25 15:13:51 +0200

    updated classification & classifier

--- classify.py
+++ classify.py
@@ -6,9 +6,10 @@ import sklearn
 import re
 
 from sklearn.feature_extraction.text import TfidfVectorizer
+features = pickle.load(open("./data/features.pkl", 'rb'))
+loaded_model = pickle.load(open("./data/GaussianProcess.pkl", 'rb'))
 
-loaded_model = pickle.load(open("gaussianProcess.pkl", 'rb'))
-
+print(features)
 df = pd.DataFrame([])
 df = pd.read_csv('cleaned_tweets.csv')
 
@@ -17,11 +18,13 @@ from sklearn.feature_extraction import text
 punc = ['.', ',', '"', "'", '?', '!', ':', ';', '(', ')', '[', ']', '{', '}',"%"]
 stop_words = text.ENGLISH_STOP_WORDS.union(punc)
 desc = df.tokens_text.values
-vectorizer = TfidfVectorizer(stop_words = stop_words,max_features = 56)
+vectorizer = TfidfVectorizer(stop_words = stop_words)
 X = vectorizer.fit_transform(desc)
 words = vectorizer.get_feature_names()
 df2 = pd.DataFrame(X.toarray(), columns=words)
+df2 = df2[features]
+print(df2)
 
-df["sentiment"] = loaded_model.predict(df2)
+df["GaussianProcess"] = loaded_model.predict(df2)
 
-df.to_csv('classified_tweetsAAA.csv', sep=',', index=False)
+df.to_csv('./data/PostClassification.csv', sep=',', index=False)
